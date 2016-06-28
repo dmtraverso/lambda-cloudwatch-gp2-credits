63388162d41f9125cca4454bb7e263836306d308
Damian Traverso <dmtraverso@gmail.com>
2016-06-28 16:04:05 -0300

    Using async

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 "use strict";
 console.log('Loading function');
 
+let async = require('async');
 let AWS = require('aws-sdk');
 let cloudwatch = new AWS.CloudWatch();
 let ec2 = new AWS.EC2();
+
+// Variables 
 const period = 3600;
 var counters = [];
 
@@ -13,13 +16,13 @@ function now() {
     return Math.round(d.getTime() / 1000);
 }
 
-// Get Read and Write metrics for each volume
-function getMetrics(vol, callback) {
-
+function getVolumeMetric(volume_id, metric, callback) {
     var end_time = now();
+
+    // Prepare params variable for CloudWatch
     var params = {
-        EndTime: end_time,                 
-        MetricName: 'VolumeReadOps',
+        MetricName : metric,
+        EndTime: end_time,
         Namespace: 'AWS/EBS',
         Period: period,
         StartTime: end_time - period,
@@ -28,19 +31,54 @@ function getMetrics(vol, callback) {
         ],
         Dimensions: [{
             Name: 'VolumeId',
-            Value: vol.VolumeId
+            Value: volume_id
         }]
     }
 
+    // Call CloudWatch
     cloudwatch.getMetricStatistics(params, function(err, data) {
-        if (err) console.log(err, err.stack);   // an error occurred
-        //else     console.log(data);           // successful response
-
-        callback(data);
+        if (err) {
+            callback(err.stack);
+        }
+        else {
+            if (data.Datapoints.length !== 0) {
+                callback(null, data.Datapoints[0].Sum); 
+            }
+            else {
+                callback(0);
+            }
+        }
     });
 }
 
+// Get Read and Write metrics for each volume
+function getMetrics(vol, callback) {
+
+    async.parallel({
+        ReadOps: function(callback) {
+
+            getVolumeMetric(vol.VolumeId, 'VolumeReadOps', (data) => {
+                callback(null, data);
+            });
+        },
+
+        WriteOps: function(callback) {
+
+            getVolumeMetric(vol.VolumeId, 'VolumeWriteOps', (data) => {
+                callback(null, data);
+            });
+        } 
+    }, function(err, results) {
+
+            // results is now equals to: {one: 'abc\n', two: 'xyz\n'}
+            //console.log(results);
+            //callback(results.ReadOps + results.WriteOps);
+            callback(results.ReadOps + results.WriteOps);
+    });
+};
+
 exports.handler = (event, context, callback) => {
+
     var params = {
         Filters: [{
             Name: 'volume-type',
